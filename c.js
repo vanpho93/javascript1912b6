58b55d3d9c6e443af4980790c184e39fb8a95462
Admin <vanpho02@gmail.com>
2016-12-30 20:29:36 +0700

    xong

--- c.js
+++ c.js
@@ -1,7 +1,7 @@
-// function Person(name, age){
-//   this.name = name;
-//   this.age = age;
-// }
+function Person1(name, age){
+  this.name = name;
+  this.age = age;
+}
 //
 // extends
 
@@ -26,15 +26,37 @@ class Person {
 // console.log(p.sayHello());
 
 class Child extends Person{
+  constructor(name, age, hobby){
+    super(name, age);
+    this.hobby = hobby;
+  }
   sayHello(){
-    return `Xin chao, em la ${this.name}, em ${this.age} tuoi`;
+    return `Xin chao, em la ${this.name}, em ${this.age} tuoi, em thich choi ${this.hobby}`;
   }
   addAge(){
     this.age += 2;
   }
 }
 
-var c = new Child('Ti', 3);
+var c = new Child('Ti', 3, 'Oto');
+Child.prototype.gioitinh = 'Nu';
+console.log('CHild gioi tinh', c.gioitinh);
+console.log(c);
 c.addAge();
 c.setAge(35);
 console.log(c.sayHello());
+// var d = new Child('Teo', 4, 'May bay')
+var e = new Person1('KhoaPham', 30);
+e.weight = 50;
+var f = new Person1('Le thi rieng', 90);
+Person1.prototype.getAge = function(){
+  console.log(this.age);
+};
+
+Person1.prototype.gioitinh = 'Nam';
+console.log('Gioi tinh', f.gioitinh);
+console.log(JSON.stringify(e));
+
+//Khai bao class Point(x, y)
+//Khai bao class DoanThang(p1, p2)
+//phuong thuc getLength()
